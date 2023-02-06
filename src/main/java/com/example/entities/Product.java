4bc203d489e31ed768c2bc0056d1c5bf363c608a
MdelolmoCode <mdelolmocorreo@gmail.com>
2023-02-06 23:22:11 +0100

    creation of Product entity and repository

---
+++ src/main/java/com/example/entities/Product.java
@@ -0,0 +1,101 @@
+package com.example.entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    Double price;
+    String category;
+    @ManyToOne
+    Manufacturer manufacturer;
+    Long stockLeft;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, Double price, String category, Manufacturer manufacturer, Long stockLeft) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.manufacturer = manufacturer;
+        this.stockLeft = stockLeft;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Long getStockLeft() {
+        return stockLeft;
+    }
+
+    public void setStockLeft(Long stockLeft) {
+        this.stockLeft = stockLeft;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", stockLeft=" + stockLeft +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.repositories;
+
+import com.example.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+}
