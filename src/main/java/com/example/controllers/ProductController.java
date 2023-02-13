df0f22a913c892264ff9f91eaa77dab2a32ba620
MdelolmoCode <mdelolmocorreo@gmail.com>
2023-02-13 10:32:50 +0100

    - merge branches - add product controller - add template product-list and product-detail

---
+++ src/main/java/com/example/controllers/ProductController.java
@@ -0,0 +1,38 @@
+package com.example.controllers;
+
+import com.example.entities.Product;
+import com.example.services.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    // http://localhost:8080/products
+    @GetMapping("products")
+    public String findAll(Model model){
+        List<Product> products = productService.findAll();
+        model.addAttribute("products", products);
+        return "product-list";
+    }
+
+    @GetMapping("foods/{id}")
+    public String findById(Model model, @PathVariable Long id){
+        Optional<Product> productOpt = productService.findById(id);
+        if(productOpt.isPresent())
+            model.addAttribute("products", productOpt.get());
+        else
+            model.addAttribute("error","Product not found");
+        return "product-detail";
+    }
+
+}
