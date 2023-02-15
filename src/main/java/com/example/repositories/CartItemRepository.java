6124b58c3fa81a7487ff62ca92e7ecb85086278d
JavierGrijalba <jgrijalaz@gmail.com>
2023-02-15 20:15:31 +0100

    CartItem servicio y repositorio terminados.

--- src/main/java/com/example/repositories/CartItemRepository.java
+++ src/main/java/com/example/repositories/CartItemRepository.java
@@ -2,27 +2,20 @@ package com.example.repositories;
 
 import com.example.entities.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long>
 {
-//    List<CartItem> findAll();
-//
-//    boolean existsById(Long id);
-//    Optional<CartItem> findById(Long id);
-//
-//    List<CartItem> findByProductId(Long id);
-//    List<CartItem> findByShoppinCart(Long id);
-//
-//    List<CartItem> findByAmountIs(Long amount);
-//    List<CartItem> findByAmountGreaterThanEqual(Long minAmount);
-//    List<CartItem> findByAmountIsLessThanEqual(Long maxAmount);
-//    List<CartItem> findByAmountIsBetween(long minAmount, Long maxAmount);
-//
-//    List<CartItem> findByPriceIs(double price);
-//    List<CartItem> findByPriceIsGreaterThanEqual(double price);
-//    List<CartItem> findByPriceIsLessThanEqual(double price);
-//    List<CartItem> findByPriceIsBetween(double minPrice, double maxPrice);
+    List<CartItem> findByProductId(Long id);
+    List<CartItem> findByShoppinCart(Long id);
+
+    List<CartItem> findByAmountIs(Long amount);
+    List<CartItem> findByAmountGreaterThanEqual(Long minAmount);
+    List<CartItem> findByAmountIsLessThanEqual(Long maxAmount);
+    List<CartItem> findByAmountIsBetween(long minAmount, Long maxAmount);
+
+    List<CartItem> findByPriceIs(double price);
+    List<CartItem> findByPriceIsGreaterThanEqual(double price);
+    List<CartItem> findByPriceIsLessThanEqual(double price);
+    List<CartItem> findByPriceIsBetween(double minPrice, double maxPrice);
 }
--- src/main/java/com/example/services/CartItemServiceImpl.java
+++ src/main/java/com/example/services/CartItemServiceImpl.java
@@ -1,89 +1,120 @@
 package com.example.services;
 
 import com.example.entities.CartItem;
-
-import java.util.ArrayList;
+import com.example.repositories.CartItemRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Service
+@AllArgsConstructor
 public class CartItemServiceImpl implements CartItemService
 {
+    private final CartItemRepository cartItemRepo;
+
     @Override
     public List<CartItem> findAll()
     {
-        return null;
+        return cartItemRepo.findAll();
     }
 
     @Override
     public boolean existsById(Long id)
     {
-        return false;
+        if(id == null)
+            return false;
+
+        return cartItemRepo.existsById(id);
     }
 
     @Override
     public Optional<CartItem> findById(Long id)
     {
-        return Optional.empty();
+        return cartItemRepo.findById(id);
     }
 
     @Override
     public List<CartItem> findByProductId(Long id)
     {
-        return null;
+        return cartItemRepo.findByProductId(id);
+//        List<CartItem> cartItemsByProcduct = new ArrayList<>();
+//
+//        for (CartItem c : cartItemRepo.findAll()) {
+//            if(Objects.equals(c.getProduct().getId(), id))
+//                cartItemsByProcduct.add(c);
+//        }
+//
+//        return cartItemsByProcduct;
     }
 
     @Override
     public List<CartItem> findByShoppinCart(Long id)
     {
-        return null;
+        return cartItemRepo.findByShoppinCart(id);
+//        List<CartItem> cartItemsByShoppingCart = new ArrayList<>();
+//
+//        for (CartItem c : cartItemRepo.findAll())
+//        {
+//            if(c.getShoppingCart().getId() == id)
+//                cartItemsByShoppingCart.add(c);
+//        }
+//
+//        return cartItemsByShoppingCart;
     }
+
+    //================================================================================================
 
     @Override
     public List<CartItem> findByAmountIs(Long amount)
     {
-        return null;
+        return cartItemRepo.findByAmountIs(amount);
     }
 
     @Override
     public List<CartItem> findByAmountGreaterThanEqual(Long minAmount)
     {
-        return null;
+        if(minAmount == null || minAmount < 0)
+            return null;
+        return cartItemRepo.findByAmountGreaterThanEqual(minAmount);
     }
 
     @Override
     public List<CartItem> findByAmountIsLessThanEqual(Long maxAmount)
     {
-        return null;
+        return cartItemRepo.findByAmountIsLessThanEqual(maxAmount);
     }
 
     @Override
     public List<CartItem> findByAmountIsBetween(long minAmount, Long maxAmount)
     {
-        return null;
+        return cartItemRepo.findByAmountIsBetween(minAmount, maxAmount);
     }
+
+    //================================================================================================
 
     @Override
     public List<CartItem> findByPriceIs(double price)
     {
-        return null;
+        return cartItemRepo.findByPriceIs(price);
     }
 
     @Override
     public List<CartItem> findByPriceIsGreaterThanEqual(double price)
     {
-        return null;
+        return cartItemRepo.findByPriceIsGreaterThanEqual(price);
     }
 
     @Override
     public List<CartItem> findByPriceIsLessThanEqual(double price)
     {
-        return null;
+        return cartItemRepo.findByPriceIsLessThanEqual(price);
     }
 
     @Override
     public List<CartItem> findByPriceIsBetween(double minPrice, double maxPrice)
     {
-        return null;
+        return cartItemRepo.findByPriceIsBetween(minPrice, maxPrice);
     }
 
 
