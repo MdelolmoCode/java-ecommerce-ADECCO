5ad505b432427cc91422d7dfe00e77b1c4ec73a8
bububuu <albusor@gmail.com>
2023-02-17 02:15:55 +0100

    ShoppingCartService and ShoppingCartServiceImpl 2

--- src/main/java/com/example/repositories/CustomerRepository.java
+++ src/main/java/com/example/repositories/CustomerRepository.java
@@ -3,5 +3,10 @@ package com.example.repositories;
 import com.example.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findBySurname(String surname);
+
+    Customer findByEmail(String email);
 }
