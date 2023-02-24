8fe899e6697172e53cc6a69870919a8eb478270c
JavierGrijalba <jgrijalaz@gmail.com>
2023-02-24 12:58:10 +0100

    Antes de fusionar con master.

--- src/main/java/com/example/repositories/CartItemRepository.java
+++ src/main/java/com/example/repositories/CartItemRepository.java
@@ -7,15 +7,15 @@ import java.util.List;
 public interface CartItemRepository extends JpaRepository<CartItem, Long>
 {
     List<CartItem> findAllByProductId(Long id);
-    List<CartItem> findAllByShoppingCartId(Long id);
+    //List<CartItem> findAllByShoppingCartId(Long id);
 
     List<CartItem> findAllByAmountIs(Long amount);
     List<CartItem> findAllByAmountGreaterThanEqual(Long minAmount);
     List<CartItem> findAllByAmountIsLessThanEqual(Long maxAmount);
     List<CartItem> findAllByAmountIsBetween(long minAmount, Long maxAmount);
 
-    List<CartItem> findAllByPriceIs(double price);
-    List<CartItem> findAllByPriceIsGreaterThanEqual(double price);
-    List<CartItem> findAllByPriceIsLessThanEqual(double price);
-    List<CartItem> findAllByPriceIsBetween(double minPrice, double maxPrice);
+//    List<CartItem> findAllByPriceIs(double price);
+//    List<CartItem> findAllByPriceIsGreaterThanEqual(double price);
+//    List<CartItem> findAllByPriceIsLessThanEqual(double price);
+//    List<CartItem> findAllByPriceIsBetween(double minPrice, double maxPrice);
 }
