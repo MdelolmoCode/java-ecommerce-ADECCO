d978d2c8bb08dee5532cbffb302f844fb95e98b2
bububuu <albusor@gmail.com>
2023-02-27 09:34:17 +0100

    editar customer (volver button) - customer-form.html

--- src/main/java/com/example/controllers/CustomerController.java
+++ src/main/java/com/example/controllers/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -63,5 +64,6 @@ public class CustomerController {
 
         return "customer/customer-form";
     }
+
 }
 
