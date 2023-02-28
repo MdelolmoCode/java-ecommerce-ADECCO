197d3179f97189ca5cdd3536c55a5a7cd457ac39
JavierGrijalba <jgrijalaz@gmail.com>
2023-02-28 11:10:07 +0100

    Indices de PaymentMethod en Orders solucionado.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -142,12 +142,12 @@ VALUES (1, 1, 1, 1),
 
 TRUNCATE TABLE orders;
 INSERT INTO orders (id, order_number, payment_method, address_id, shopping_cart_id)
-VALUES (1, 1, 1, 1, 1),
-       (2, 2, 2, 2, 2),
-       (3, 3, 3, 3, 3),
-       (4, 4, 1, 4, 4),
-       (5, 5, 2, 5, 5),
-       (6, 6, 3, 6, 6)
+VALUES (1, 1, 0, 1, 1),
+       (2, 2, 1, 2, 2),
+       (3, 3, 2, 3, 3),
+       (4, 4, 0, 4, 4),
+       (5, 5, 1, 5, 5),
+       (6, 6, 2, 6, 6)
 ;
 
 SET FOREIGN_KEY_CHECKS = 1;
