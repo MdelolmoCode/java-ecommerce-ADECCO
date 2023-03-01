44e837bd38109a72e54cef55065950c22f1b7e8f
bububuu <albusor@gmail.com>
2023-03-01 12:56:35 +0100

    rename /shopping-carts

--- src/main/java/com/example/controllers/ShoppingCartController.java
+++ src/main/java/com/example/controllers/ShoppingCartController.java
@@ -15,7 +15,7 @@ public class ShoppingCartController {
 
     private final ShoppingCartService shoppingCartService;
 
-    @GetMapping("/shoppingCarts") // Puede ser getmapping para recurar algo o PostMapping para enviar
+    @GetMapping("/shopping-carts") // Puede ser getmapping para recurar algo o PostMapping para enviar
     public String findAll(Model model){
 
         List<ShoppingCart> shoppingCarts = shoppingCartService.findAll();
